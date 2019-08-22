7b82d509ead48499da7078327439059bdf39aa9b
dougluz <dougs.2210@gmail.com>
2019-08-22 15:00:28 -0300

    aplicativo finalizado

---
+++ src/services/realm.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+import Realm from 'realm';
+
+import RepositorySchema from '../schemas/RepositorySchema';
+
+export default function getRealm() {
+  return Realm.open({
+    schema: [RepositorySchema],
+  });
+}
